415df1271758d645a5a2c351e3b365d1ca8682ca
Piotr Kardaś <pkardas.it@gmail.com>
2020-07-20 22:31:54 +0200

    Assigned master document

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Assign the master document
+# https://www.sphinx-doc.org/en/master/usage/configuration.html?highlight=master_doc
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
